1e50278e3dea0f25f43aa833b854ee1c943c1bf2
GabrielLicar <paulogabrielicar@gmail.com>
2022-11-23 09:58:02 -0300

    Project Created

---
+++ backend/src/main/java/com/gablicar/meta/MetaApplication.java
@@ -0,0 +1,13 @@
+package com.gablicar.meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaApplication.class, args);
+	}
+
+}
